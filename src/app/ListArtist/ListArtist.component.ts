336368925c47053de3f4419a7cc2dadb812f0959
Ilias <2277205@cegepmontpetit.ca>
2024-10-02 15:27:32 -0400

    Compléter service pour Artiste
    Complétion du code dans le service spotify, le type script et le html de artiste pour rechercher des artistes

--- src/app/ListArtist/ListArtist.component.ts
+++ src/app/ListArtist/ListArtist.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../services/Spotify/spotify.service';
+import { Artist } from '../../models/artist';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ListArtist',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './ListArtist.component.html',
   styleUrls: ['./ListArtist.component.css']
 })
 export class ListArtistComponent implements OnInit {
 
-  constructor() { }
+  artistName : string =""
+  artist ?: Artist;
+  jsonData : string | null= null;
 
-  ngOnInit() {
+  constructor(public spotify : SpotifyService) {
+
+   }
+
+  ngOnInit():void {
+    this.spotify.connect()
+    this.jsonData = localStorage.getItem("artiste");
+    if(this.jsonData != null){
+      this.artist = JSON.parse(this.jsonData);
+    }
+    
+  }
+  async getArtist() : Promise<void>{
+    this.artist = await this.spotify.searchArtist(this.artistName)
+    
   }
 
 }
